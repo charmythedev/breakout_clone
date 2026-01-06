d363fdfb1639f14516899b3d85b8d6f36e2a43ae
Charmy <charmythedev@gmail.com>
2026-01-05 22:02:56 -0500

    Add different colors for bricks, add "bomb" bricks, and "superball" powerup for hitting purple bricks

--- managers/ball.py
+++ managers/ball.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.sprite import collide_rect
+import random
 
 
 class Ball:
@@ -10,6 +11,9 @@ class Ball:
         self.width = 5
         self.height = 5
         self.speed = 6
+        self.timer = 0
+        self.color = (255,255,255)
+        self.superball = False
 
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
@@ -20,10 +24,17 @@ class Ball:
     def draw(self):
         pygame.draw.rect(
             self.window,
-            (255, 255, 255),
+            self.color,
             self.rect
         )
     def move(self, paddle, bricks):
+        if self.superball:
+            self.timer -=1
+            if self.timer <= 0:
+
+                self.superball = False
+                self.color = (255,255,255)
+
         self.rect.x += self.dx
         self.rect.y += self.dy
 
@@ -35,8 +46,24 @@ class Ball:
         if self.rect.colliderect(paddle.rect):
             self.dy *= -1
             self.rect.bottom = paddle.rect.top
+
         for brick in bricks[:]:
             if self.rect.colliderect(brick.rect):
-                bricks.remove(brick)
-                self.dy *= -1
-                break
+
+                if brick.color == brick.colors[0]:
+                    bricks[:] = [b for b in bricks if b.color != brick.colors[0]]
+                    print("bomb!")
+
+                elif brick.color == brick.colors[1]:
+                    print("SUPERBALL")
+                    bricks.remove(brick)
+                    self.superball = True
+                    self.timer = 120
+                    self.color = random.choice(brick.colors)
+
+
+                else:
+                    bricks.remove(brick)
+                if not self.superball:
+                    self.dy *= -1
+                    break
--- managers/bricks.py
+++ managers/bricks.py
@@ -1,11 +1,21 @@
 import pygame
+import random
 
 
 class Brick:
     def __init__(self, x, y):
         self.width = 50
         self.height = 20
+        self.colors =[(255, 18, 0),
+                      (167, 34, 156),
+                      (0, 141, 213),
+                      (238,180,179),
+                      (78,83,64)
+
+        ]
+        self.color = random.choice(self.colors)
         self.rect = pygame.Rect(x, y, self.width, self.height)
 
+
     def draw(self, window):
-        pygame.draw.rect(window, (200, 200, 200), self.rect)
+        pygame.draw.rect(window,self.color, self.rect)
